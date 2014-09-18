317977068dab529f806af25c1640ada253f32db2
jianmingtang <jian.ming.tang@gmail.com>
2014-09-17 20:48:05 -0400

    update classical list view

--- mypub/urls.py
+++ mypub/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 from mypub import views
 
 urlpatterns = patterns('', url(r'^$', views.index, name='index'),
-	url(r'^(?P<mypub_id>\d+)/$', views.detail, name='detail'),
+	url(r'^(?P<aid>\d+)/$', views.detail, name='detail'),
 )
--- mypub/views.py
+++ mypub/views.py
@@ -1,8 +1,8 @@
 #from django.shortcuts import render
 
-from mypub.models import *
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from mypub.models import Article, Journal, URL, VJ
 import re
 
 vjlist = { '0' : 'Virtual Journal of ',
@@ -12,7 +12,13 @@ vjlist = { '0' : 'Virtual Journal of ',
         }
 vjlist_keys = vjlist.keys()
 
-def fix_HTML(s):
+def toFullName(vlist):
+	for v in vlist:
+		v.title = vjlist['0'] + vjlist[v.title]
+	return vlist
+	
+
+def toHTML(s):
 	s = re.sub(r'\$(.*?)_{(.*?)}(.*?)\$', r'$\1<sub>\2</sub>\3$',s)
 	s = re.sub(r'\$(.*?)\^{(.*?)}(.*?)\$', r'$\1<sup>\2</sup>\3$',s)
 	s = re.sub(r'\$(.*?)\$', r'<i>\1</i>',s)
@@ -30,20 +36,27 @@ def index(request):
 	plist = []
 	for j in jlist:
 		a = Article.objects.filter(journal_id=j.id)[0]
-		a.title = fix_HTML(a.title)
-		a.author = fix_HTML(a.author)
-		a.abstract = fix_HTML(a.abstract)
+		a.title = toHTML(a.title)
+		a.author = toHTML(a.author)
+		a.abstract = toHTML(a.abstract)
 		ulist = URL.objects.filter(article_id=a.id).order_by('id')
-		vlist = VJ.objects.filter(article_id=a.id)
-		for v in vlist:
-			v.title = vjlist['0'] + vjlist[v.title]
+		vlist = VJ.objects.filter(article_id=a.id).order_by('id')
+		vlist = toFullName(vlist)
 		plist.append([a,j,ulist,vlist])
 	template = loader.get_template('mypub/index.html')
-	context = RequestContext(request, {'plist': plist,})
+	context = RequestContext(request, {'plist': plist})
 	return HttpResponse(template.render(context))
 
-def detail(request, mypub_id):
-	paper = Article.objects.get(id=article_id)
+def detail(request, aid):
+	a = Article.objects.get(id=aid)
+	a.title = toHTML(a.title)
+	a.author = toHTML(a.author)
+	a.abstract = toHTML(a.abstract)
+	j = Journal.objects.get(id=a.journal_id)
+	ulist = URL.objects.filter(article_id=a.id).order_by('id')
+	vlist = VJ.objects.filter(article_id=a.id).order_by('id')
+	vlist = toFullName(vlist)
+	paper = [a, j, ulist, vlist]
         template = loader.get_template('mypub/detail.html')
-        context = RequestContext(request, {'paper': paper})
+        context = RequestContext(request, {'paper': [paper]})
         return HttpResponse(template.render(context))
