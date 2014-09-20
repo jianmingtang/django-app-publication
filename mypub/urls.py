40c786d70681c871dde92fe0e3600b2060e0651c
jianmingtang <jian.ming.tang@gmail.com>
2014-09-20 01:37:58 -0400

    add search function

--- mypub/urls.py
+++ mypub/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, url
 from mypub import views
 
 urlpatterns = patterns('', url(r'^$', views.index, name='index'),
-	url(r'^(?P<aid>\d+)/$', views.detail, name='detail'),
+	url(r'^(?P<aid>\d+)/$', views.detail),
+	url(r'^search$', views.search),
 #	url(r'^meta$', views.display_meta),
 #	url(r'^search-form$', views.search_form),
-#	url(r'^search$', views.search),
 )
--- mypub/views.py
+++ mypub/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.db.models import Q
 from mypub.models import Article, Journal, URL, VJ
 import re
 
@@ -30,7 +31,7 @@ def toHTML(s):
 	s = s.replace(u'\u00c7','&Ccedil;')
 	return s
 
-def extract_one_article(a):
+def get_all_extra_links(a):
 	a.title = toHTML(a.title)
 	a.author = toHTML(a.author)
 	a.abstract = toHTML(a.abstract)
@@ -39,6 +40,13 @@ def extract_one_article(a):
 	vl = toFullName(vl)
 	return a, ul, vl
 
+def get_one_paper_from_aid(aid):
+	a = Article.objects.get(id=aid)
+	j = Journal.objects.get(id=a.journal_id)
+	a, ulist, vlist = get_all_extra_links(a)
+	t = get_thumb_name(ulist)
+	return a, j, ulist, vlist, t
+
 def get_thumb_name(ulist):
 	for u in ulist:
 		if u.name == '[Full Text]':
@@ -46,23 +54,52 @@ def get_thumb_name(ulist):
 	return t
 
 def index(request):
-	jlist = Journal.objects.order_by('-year')
+#	jlist = Journal.objects.order_by('-year')
+	alist = Article.objects.order_by('-journal__year')
 	plist = []
-	for j in jlist:
-		a = Article.objects.filter(journal_id=j.id)[0]
-		a, ulist, vlist = extract_one_article(a)
-		t = get_thumb_name(ulist)
-		plist.append([a,j,ulist,vlist,t])
+#	for j in jlist:
+	for a in alist:
+		plist.append(get_one_paper_from_aid(a.id))
+#		a = Article.objects.filter(journal_id=j.id)[0]
+#		a, ulist, vlist = get_all_extra_links(a)
+#		t = get_thumb_name(ulist)
+#		plist.append([a,j,ulist,vlist,t])
 	template = loader.get_template('mypub/index.html')
 	context = RequestContext(request, {'plist': plist})
 	return HttpResponse(template.render(context))
 
 def detail(request, aid):
-	a = Article.objects.get(id=aid)
-	j = Journal.objects.get(id=a.journal_id)
-	a, ulist, vlist = extract_one_article(a)
-	t = get_thumb_name(ulist)
-	paper = [a, j, ulist, vlist, t]
+	paper = get_one_paper_from_aid(aid)
         template = loader.get_template('mypub/detail.html')
         context = RequestContext(request, {'paper': [paper]})
         return HttpResponse(template.render(context))
+
+def search(request):
+	alist = []
+	ls = 0
+	slist = []
+	if 's' not in request.GET:
+		message = 'You submitted an empty form.'
+	else:
+		s = request.GET['s']
+		message = 'You searched for: %r' % s
+		ls = len(s)
+		if ls == 0:
+			message = 'You submitted an empty string.'
+		else:
+			slist = s.split()
+	if ls > 20:
+		message = 'Your search string is too long.'
+	else:
+		q = Q()
+		for sub in slist:
+			q = q | Q(title__icontains=sub) \
+			| Q(author__icontains=sub) | Q(abstract__icontains=sub)
+			alist = Article.objects.filter(q).order_by('-journal__year')
+		message = message + '; returning %d records.' % len(alist)
+	plist = []
+	for a in alist:
+		plist.append(get_one_paper_from_aid(a.id))
+	template = loader.get_template('mypub/index.html')
+	context = RequestContext(request, {'plist': plist, 'msg': message,})
+	return HttpResponse(template.render(context))
