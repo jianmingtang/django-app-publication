942cf240ff00746cb94f76eee89d45da9a8b658f
jianmingtang <jian.ming.tang@gmail.com>
2014-09-20 10:46:25 -0400

    fix root dir

--- mypub/urls.py
+++ mypub/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 
 from mypub import views
 
-urlpatterns = patterns('', url(r'^$', views.index, name='index'),
+urlpatterns = patterns('', url(r'^$', views.index, name='mypub'),
 	url(r'^(?P<aid>\d+)/$', views.detail),
 	url(r'^search$', views.search),
 #	url(r'^meta$', views.display_meta),
