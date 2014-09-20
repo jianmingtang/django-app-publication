1c558a9f9bebd36447557426f7c3194f4b869ede
jianmingtang <jian.ming.tang@gmail.com>
2014-09-20 14:47:43 -0400

    css animation

--- mypub/urls.py
+++ mypub/urls.py
@@ -6,5 +6,4 @@ urlpatterns = patterns('', url(r'^$', views.index, name='mypub'),
 	url(r'^(?P<aid>\d+)/$', views.detail),
 	url(r'^search$', views.search),
 #	url(r'^meta$', views.display_meta),
-#	url(r'^search-form$', views.search_form),
 )
